ec9f99aa010ddff95a3b17bfdaf99a66a2b6b11b
falseaxiom <mlegaspi@princeton.edu>
2021-11-28 03:02:33 -0500

    update index pic

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
-/* access localstorage, create level if necessary */
+/* access localstorage, create level if necessary; randomize bug image */
+const bugs = ["logo.png", "brackets.gif", "semicolon.png", "typo.png", "thimis.png"];
 window.onload = (e) =>{
     let lvl = parseInt(localStorage.getItem("lvl"));
     if (!isNaN(lvl)) {
@@ -8,4 +9,8 @@ window.onload = (e) =>{
         console.log('new player');
         localStorage.setItem("lvl","0");
     }
+
+    let rand = Math.floor(Math.random() * bugs.length);
+    let randBug = document.getElementById("randBug");
+    randBug.src = "imgs/"+bugs[rand];
 }
